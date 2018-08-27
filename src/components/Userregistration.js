b00b0cb84288fd8c234da21df6193afa51331fd1
Oleg Pukhaev <nuba8184@gmail.com>
2018-08-27 16:03:55 +0600

    update account email info

--- src/components/Userregistration.js
+++ src/components/Userregistration.js
@@ -1,90 +1,98 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 // import {connect} from 'react-redux';
-
+// var obj;
 var Auth = require('../../node_modules/j-toker/src/j-toker.js');
-var PubSub = require('../../node_modules/pubsub-js/src/pubsub.js');
+// var PubSub = require('../../node_modules/pubsub-js/src/pubsub.js');
 
 Auth.configure({
   apiUrl:                'https://floating-atoll-63112.herokuapp.com/api',
-  signOutPath:           '/auth/sign_out',
-  emailSignInPath:       '/auth/sign_in',
-  emailRegistrationPath: '/auth',
-  accountUpdatePath:     '/auth',
-  accountDeletePath:     '/auth',
-  passwordResetPath:     '/auth/password',
-  passwordUpdatePath:    '/auth/password',
-  tokenValidationPath:   '/auth/validate_token',
-  proxyIf:               function() { return false; },
-  proxyUrl:              '/proxy',
-  validateOnPageLoad:    false,
-  forceHardRedirect:     false,
-  storage:               'cookies',
-  cookieExpiry:          14,
-  cookiePath:            '/',
-
-  passwordResetSuccessUrl: function() {
-    return window.location.href;
-  },
-
-  confirmationSuccessUrl:  function() {
-    return window.location.href;
-  },
-
-  tokenFormat: {
-    "access-token": "{{ access-token }}",
-    "token-type":   "Bearer",
-    client:         "{{ client }}",
-    expiry:         "{{ expiry }}",
-    uid:            "{{ uid }}"
-  },
-
-  parseExpiry: function(headers){
-    // convert from ruby time (seconds) to js time (millis)
-    return (parseInt(headers['expiry'], 10) * 1000) || null;
-  },
-
-  handleLoginResponse: function(resp) {
-    return resp.data;
-  },
-
-  handleAccountUpdateResponse: function(resp) {
-    return resp.data;
-  },
-
-  handleTokenValidationResponse: function(resp) {
-    return resp.data;
-  },
-
-  authProviderPaths: {
-    github:    '/auth/github',
-    facebook:  '/auth/facebook',
-    google:    '/auth/google_oauth2'
-  }
 });
 
 class Userregistration extends React.Component {
-    
+
+  
+
   constructor () {
-    super();
+    super()
+
+    this.state = { 
+      user_full_name : "User",
+      user_email : null,
+      address: null
+    } 
+
+    this.userLogin = this.userLogin.bind(this)
+    this.userRegistr = this.userRegistr.bind(this)
+    this.signOut = this.signOut.bind(this)
+    this.onClickUpdateAcc = this.onClickUpdateAcc.bind(this)
   }
 
   userRegistr () {
-
     Auth.emailSignUp({
+      first_name: 'Oleg',
+      last_name: 'Pukhaev',
       email: 'nuba8184@gmail.com',
-      password: '123456789',
-      password_confirmation: '123456789'
+      password: '123456789'
     });
-
   }
 
+  userLogin () {
+    Auth.emailSignIn({
+      email: 'nuba818400@gmail.com',
+      password: "123456789"
+    });
+
+    var obj = Object.assign({}, Auth );
+    this.setState({
+      user_full_name: obj.user.full_name,
+      user_email: obj.user.email,
+      user_address: obj.user.address            
+    });
+
+    console.log(Auth.user);
+  }
+
+  signOut () {
+    Auth.signOut();
+  }
+
+  onClickUpdateAcc () {
+    Auth.updateAccount({
+      address: 'nuba8184@gmail.com',
+      password: "123456789"
+    });
+    console.log(Auth.user);
+  }
+  
+
   render() {
+
     return (
       <div>
-        <input type="text" placeholder="Email"></input>
-        <input type="text" placeholder="Password"></input>
-        <button onClick={this.userRegistr}>Sign up</button>
+        <div>
+          <h3>Registration</h3>
+          <input type="text" placeholder="Email"></input>
+          <input type="text" placeholder="Password"></input>
+          <button onClick={this.userRegistr}>Sign up</button>
+        </div>
+        <div>
+          <h3>Sign in</h3>
+          <input type="text" placeholder="Email"></input>
+          <input type="text" placeholder="Password"></input>
+          <button onClick={this.userLogin}>Sign in</button>
+          <button onClick={this.signOut}>Sign out</button>
+
+          <h2>Welcome {this.state.user_full_name}</h2>
+          <h2>E-mail {this.state.user_email}</h2>
+          <h2>Address {this.state.address}</h2>
+          <br></br>
+          <button onClick={this.onClickUpdateAcc}>update accaunt</button>
+
+
+
+
+
+        </div>
       </div>
     );
   }
