a38c7547c911d60d12e3867fe9e9b89a966da046
Oleg Pukhaev <nuba8184@gmail.com>
2018-09-03 17:57:45 +0600

    Right coll menu added

--- src/components/Main.js
+++ src/components/Main.js
@@ -101,12 +101,79 @@ class Main extends React.Component {
     			</section>
     		</nav>
         
-        
-        
-        
-        
-        
-        
+        <div class="content">
+          <div class="container-fluid">
+
+            <div class="panel panel-default my-main-panel">
+              <div class="panel-body">
+                <div class="flexbox">
+
+<div class="right-col">
+								{/* <!-- Nav tabs --> */}
+								<ul class="nav nav-pills nav-stacked my-sidebar" role="tablist">
+									<li role="presentation">
+										<a href="#overview" aria-controls="overview" role="tab" data-toggle="tab">
+											<span class="icon icon-overview"></span>
+											Overview
+										</a>
+									</li>
+									<li role="presentation">
+										<a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">
+											<span class="icon icon-message"></span>
+											Messages
+										</a>
+									</li>
+									<li role="presentation">
+										<a href="#my-jobs" aria-controls="my-jobs" role="tab" data-toggle="tab">
+											<span class="icon icon-jobs"></span>
+											My Jobs
+										</a>
+									</li>
+									<li role="presentation">
+										<a href="#my-promo" aria-controls="my-promo" role="tab" data-toggle="tab">
+											<span class="icon icon-promo"></span>
+											My Promo
+										</a>
+									</li>
+									<li role="presentation">
+										<a href="#skills" aria-controls="skills" role="tab" data-toggle="tab">
+											<span class="icon icon-skills"></span>
+											My Skills
+										</a>
+									</li>
+									<li role="presentation" class="active">
+										<a href="#saved" aria-controls="saved" role="tab" data-toggle="tab">
+											<span class="icon icon-saved"></span>
+											Saved
+										</a>
+									</li>
+									<li role="presentation">
+										<a href="#media" aria-controls="media" role="tab" data-toggle="tab">
+											<span class="icon icon-media"></span>
+											My Media
+										</a>
+									</li>
+									<li role="presentation">
+										<a href="#accounts" aria-controls="accounts" role="tab" data-toggle="tab">
+											<span class="icon icon-accounts"></span>
+											Accounts
+										</a>
+									</li>
+									<li role="presentation">
+										<a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">
+											<span class="icon icon-profile"></span>
+											Profile
+										</a>
+									</li>
+								</ul>
+						 </div>	{/* <!--right-col End--> */}
+
+                </div>
+              </div>
+             </div> 
+
+          </div>
+        </div>
         {/* /// */}
         
 
