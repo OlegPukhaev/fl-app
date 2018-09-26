3fbeccf082920bd771f24ffe43848926e1244802
Oleg Pukhaev <nuba8184@gmail.com>
2018-09-26 13:39:01 +0600

    Fixed axios.defaults.headers.common now working from cookies

--- src/functions/api.js
+++ src/functions/api.js
@@ -1,35 +1,24 @@
 import Reactotron from 'reactotron-react-js';
 import {getToken} from '../functions/config';
 import axios from 'axios';
-var configapi = getToken();
+// var configapi = getToken();
+import queryString from 'query-string';
+// const queryString = require('query-string');
 
-axios.defaults.headers.common['access-token'] = "NjSJexJN8cQCTRN-0Oj9SQ";
-axios.defaults.headers.common['expiry'] = "1539153539";
-axios.defaults.headers.common['token-type'] = "Bearer";
-axios.defaults.headers.common['uid'] = "nuba818400@gmail.com";
-axios.defaults.headers.common['client'] = "y0Hffjly56_06rlgJSpcdw";
-// Reactotron.log("cookie", queryString.parse(token.authHeaders));
-
-const queryString = require('query-string');
-// import queryString from 'query-string';
-
-
+axios.defaults.headers.common = tokobj;
 axios.defaults.baseURL = 'https://floating-atoll-63112.herokuapp.com';
 
+var token = queryString.parse(document.cookie);
+var tokenParsed = JSON.parse(token.authHeaders);
 
-var token = queryString.parse(document.cookie)
-// axios.defaults.headers.common['Authorization'] = {
-//     "access-token":"NjSJexJN8cQCTRN-0Oj9SQ",
-//     "token-type":"Bearer",
-//     "client":"y0Hffjly56_06rlgJSpcdw",
-//     "expiry":"1539153539",
-//     "uid":"nuba818400@gmail.com"};
+const tokobj = tokenParsed;
+
 
 
 
 
 export function getData(urlApi) {
-    console.log("fff"+token.authHeaders);
+    // console.log("fff"+token.authHeaders);
     return axios.get(urlApi)
     .then(response => {
         let dataObj = Object.assign({}, response);
