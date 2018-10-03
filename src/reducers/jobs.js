23031fb77d2dfc2b8ea1b385087439ee922ddf99
Oleg Pukhaev <nuba8184@gmail.com>
2018-10-03 14:48:26 +0600

    Jobs reduser modal win toggler

---
+++ src/reducers/jobs.js
@@ -0,0 +1,39 @@
+import Reactotron from 'reactotron-react-js';
+
+const	SHOW_MODAL_WIN = 'SHOW_MODAL_WIN';
+
+let initialState = {
+  modalWinToggler = "display:none"
+}
+
+export function winToggler(value) {
+  if (value === true) {
+    var typeToggler = SHOW_MODAL_WIN;
+    var valueToggler = "display:block";
+  } else {
+    var typeToggler = SHOW_MODAL_WIN;
+    var valueToggler = "display:none";
+  }
+  return dispatch => {
+    dispatch({
+      type: typeToggler,
+      payload: valueToggler 
+    });
+  };
+}
+
+const actionsMap = {
+	[SHOW_MODAL_WIN]: (state, action) => {
+		return {
+      ...state, 
+      modalWinToggler: action.payload
+    }
+	},
+}
+
+
+export default function jobs(state = initialState, action) {
+  const reduceFn = actionsMap[action.type];
+  if (!reduceFn) return state;
+  return reduceFn(state, action);
+}
